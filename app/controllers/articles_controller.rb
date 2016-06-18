eb368a59dd4345310b9100ab94f85425d9355401
Francis first blog <francischin@myimart.com>
2016-06-18 04:43:50 +0000

    update show, edit and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+      @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
@@ -15,6 +19,26 @@ class ArticlesController < ApplicationController
         render 'new'  
     end
   end
+  
+  def edit
+    @article = Article.find(params[:id])  
+  end
+  
+  def update
+  
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article is successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+      
+      
+    
+  end
+  
+  
   
   def show
     @article = Article.find(params[:id])  
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root 'articles#index'
   
   resources :articles
 
